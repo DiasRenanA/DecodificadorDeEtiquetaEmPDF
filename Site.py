05711a7c0899087b4380efc90c4f163061000f54
DiasRenanA <149257879+DiasRenanA@users.noreply.github.com>
2024-05-26 20:53:55 -0300

    Update Site.py 1.1.0

--- Site.py
+++ Site.py
@@ -3,13 +3,13 @@ import base64
 import os
 
 class Site:
-    # ------------------ Funções ----------------
     def DownloadPDF(self):
         self.Descriptografar()
         if self.pdf_string01 == "%":
             caminho_pdf = self.temp_pdf_file
             with open(caminho_pdf, "rb") as pdf_file:
                 pdf_bytes = pdf_file.read()
+            st.write("PDF decodificado!")
             st.download_button(
                 label='Baixar PDF',
                 data=pdf_bytes,
@@ -31,19 +31,29 @@ class Site:
         except Exception as e:
             st.error(f"Erro ao descriptografar o texto Base64: {e}")
 
-    # --------------------------------------------
     def __init__(self):
         self.pdf_string01 = ''
         self.temp_pdf_file = ''
-        with st.container():  # Cabeçalho da página
+        with st.container():
             st.subheader("DECODIFICADOR DE PDF EM BASE64")
             st.write("Preencha os campos abaixo para ter seu PDF decodificado")
 
-        with st.container():  # Campo para preencher
+        with st.container():
             self.NomePDF = st.text_input("Digite um nome para o PDF:")
             self.Base64_str = st.text_area("Cole aqui o texto em Base64: ", height=400)
-            if st.button('Descriptografar e Baixar PDF'):
-                self.DownloadPDF()
+            if st.button('Validar'):
+                self.ValidarInputs()
+
+    def ValidarInputs(self):
+        if not self.NomePDF:
+            st.error("Erro: O campo 'Digite um nome para o PDF' não pode estar vazio.")
+        elif not self.Base64_str:
+            st.error("Erro: O campo 'Cole aqui o texto em Base64' não pode estar vazio.")
+        elif len(self.Base64_str) > 100000:
+            st.error("Erro: O texto em Base64 excede o limite de 100000 caracteres.")
+        else:
+            self.DownloadPDF()
+
 
 if __name__ == "__main__":
     Site()
